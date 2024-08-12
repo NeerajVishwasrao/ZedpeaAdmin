012bb85d4d718d36544586fdf43ded7b840a435f
Niraj Vishwasrao <neerajvs2001@gmail.com>
2024-08-12 23:22:50 +0530

    Independent searched on each coloumn added

--- src/app/exam-section/create-exam/create-exam.component.ts
+++ src/app/exam-section/create-exam/create-exam.component.ts
@@ -12,28 +12,51 @@ import { SearchexamPipe } from './searchexam.pipe';
   styleUrl: './create-exam.component.css'
 })
 export class CreateExamComponent {
-  filteredQuestions: any[] = [];
+  filteredQuestions: question[] = [];
 
-  questiondatabase: any = ''
+  questiondatabase: question[] = [];
   http = inject(HttpClient);
   searchtext: string = ''
   searchtext1: string = ''
   searchtext2: string = ''
+  searchtext3: string = ''
+  searchtext4: string = ''
+
 
   ngOnInit() {
     console.log("before http get")
-    this.http.get("/assets/questiondatabase.json").subscribe(data => {
+    this.http.get<question[]>("/assets/questiondatabase.json").subscribe(data => {
       console.log(data)
-      this.questiondatabase = data
+      this.questiondatabase = data;
+      this.filteredQuestions=data;
     })
+    // this.filterQuestions=this.questiondatabase
 
   }
   
   
-  // filterQuestions() {
-  //   this.filteredQuestions = this.questiondatabase.filter(Qcontainer =>
-  //     Qcontainer.std.toLowerCase().includes(this.searchtext1.toLowerCase()) &&
-  //     Qcontainer.template.toLowerCase().includes(this.searchtext2.toLowerCase())
-  //   );
-  // }
+  filterQuestions() {
+   // Return original data if any search text is empty
+   // in js empty string is declared as false;
+   //include cheaks if substring is present is string or not
+   //qcontainer indicated first element of questiondatabase array
+   // ctrl + shift + p for emoji
+   console.log(this.searchtext1)
+   if (!this.searchtext1 && !this.searchtext2 && !this.searchtext3 && !this.searchtext4) {
+    this.filteredQuestions = this.questiondatabase;
+  }
+    this.filteredQuestions = this.questiondatabase.filter(Qcontainer =>
+      Qcontainer.std.toLowerCase().includes(this.searchtext1.toLowerCase()) &&
+      Qcontainer.sub.toLowerCase().includes(this.searchtext2.toLowerCase())&&
+      Qcontainer.template.toLowerCase().includes(this.searchtext3.toLowerCase())&&
+      Qcontainer.question.toLowerCase().includes(this.searchtext4.toLowerCase())
+
+    );
+  }
+}
+interface question{
+  std:string;
+  sub:string;
+  template:string;
+  question:string;
 }
