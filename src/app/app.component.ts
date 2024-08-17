3b2a5f8e39790671dfd084225624312f2484c71a
Akshadabangar <akshadabangar21052000@gmail.com>
2024-08-17 14:58:16 +0530

    akshada UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Zedpea_Admin';
+  title = 'Teachers Dashboard';
 }
