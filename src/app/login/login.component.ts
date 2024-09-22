6b0cd826c9aa2f388b3e79fa89d3b4cd91054746
NeerajVishwasrao <neerajvishwasrao@gmail.com>
2024-09-22 22:21:52 +0530

    formatted

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,35 +10,36 @@ import { Loader2Component } from '../Reusable-view/loader-2/loader-2.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,CommonModule, NgClass,LoaderComponent,Loader2Component],
+  imports: [FormsModule, CommonModule, NgClass, LoaderComponent, Loader2Component],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-isLoaderActive: boolean=true;
-isLogging:  boolean=false;;
-togglePasswordVisibility() {
-this.showPassword = !this.showPassword}
+  isLoaderActive: boolean = true;
+  isLogging: boolean = false;;
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword
+  }
   router = inject(Router);
   leaguelist: Array<any> = [];
-showPassword: boolean=false;
+  showPassword: boolean = false;
 
   ngOnInit() {
     // localStorage.setItem("uprofile","tejas")
 
-    if (localStorage.getItem("uprofile")!=null) {
+    if (localStorage.getItem("uprofile") != null) {
       this.router.navigateByUrl("Exam-Center/AddExam")
-    }else{
+    } else {
       this.loginService.league()
-      .subscribe(
-        (res) => {
-          console.log(res);
-          this.leaguelist = res;
-          this.isLoaderActive=false
-        }
-      );
+        .subscribe(
+          (res) => {
+            console.log(res);
+            this.leaguelist = res;
+            this.isLoaderActive = false
+          }
+        );
     }
-    
+
   }
   LoginCredentials: { email: string; password: string } = {
     email: "",
@@ -54,27 +55,27 @@ showPassword: boolean=false;
 
 
   verify() {
-this.isLogging=true
+    this.isLogging = true
     let user = <LUser>{};
-    user = { 
+    user = {
       leagueName: this.LoginCredentials.email,
       password: this.LoginCredentials.password
     }
 
     this.loginService.myleague(user)
-    .subscribe(
-      (res) => {
-        this.isLogging=false
-        console.log(res);
-        this.response = res;
-        if (this.response.length) {
-          localStorage.setItem("uprofile",JSON.stringify(this.response[0]))
+      .subscribe(
+        (res) => {
+          this.isLogging = false
+          console.log(res);
+          this.response = res;
+          if (this.response.length) {
+            localStorage.setItem("uprofile", JSON.stringify(this.response[0]))
             this.router.navigateByUrl("Exam-Center/AddExam")
-         } else {
+          } else {
             alert("Invalid Credentials")
-         }
-      }
-    );
+          }
+        }
+      );
 
   }
 
